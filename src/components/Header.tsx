21f38564a0f5f9d12c7d4001b2643f1a74d19119
Jackson <jpkubin@wm.edu>
2023-10-19 10:48:50 -0400

    improve header mobile responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ const Header: FC<{
     children: ReactNode
 }> = ({image: src, children}) => {
     return (
-        <header className="flex flex-col md:flex-row items-center gap-4 mt-8">
+        <header className="flex flex-col sm:flex-row items-center gap-4 mt-8">
             <img src={src} className="w-40 rounded-full border-amber-900 border-4" />
-            <div className="flex-1 px-4 flex flex-col gap-2 justify-center items-start">
+            <div className="flex-1 px-4 flex flex-col gap-2 justify-center items-center sm:items-start">
                 {children}
             </div>
         </header>
